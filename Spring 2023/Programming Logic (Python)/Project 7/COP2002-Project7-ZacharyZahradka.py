4bbab18a67248a208f659037680945eb6cab54d6
zderApfel <scoutt172@yahoo.com>
2023-04-28 12:45:08 -0400

    Completed Project 7

--- Spring 2023/Programming Logic (Python)/Project 7/COP2002-Project7-ZacharyZahradka.py
+++ Spring 2023/Programming Logic (Python)/Project 7/COP2002-Project7-ZacharyZahradka.py
@@ -1,22 +1,55 @@
 '''
     Zachary Zahradka
     COP2002.0M1
-    04/25/2023
+    04/28/2023
     Project 7: Files and Classes
 '''
 class JSONObject:
-    def __init__(self,keys,value):
+    def __init__(self,keys,values,file):
         self.keys = keys
-        self.value = value
-        self.item = self.makeItem()
+        self.values = values
+        self.objects = []
+        self.file = file
 
-    def makeItem(self):
-        i = 0
-        newObj = {}
-        while i < len(self.keys):
-            newObj[self.keys[i]] = self.value[i]
-            i+=1
-        return newObj
+    def writeToJSON(self):
+        for value in self.values:
+            i = 0
+            newObj = {}
+            while i < len(self.keys):
+                newObj[self.keys[i]] = value[i]
+                i+=1
+            self.objects.append(newObj)
+        self.makeParsable()
+
+        with open(self.file, 'w') as file_object:
+            file_object.write('{\n\t')
+            i = 0
+            while i < len(self.objects):
+                o = self.objects[i]
+                file_object.write('"'+str(i+1)+'": {\n')
+                i+=1
+                if i == len(self.objects):
+                    file_object.write('\t\t'+o+'\n\t}'+'\n')
+                    file_object.write('}')
+                else:
+                    file_object.write('\t\t'+o+'\n\t},'+'\n\t')
+                
+
+    def makeParsable(self):
+        new = []
+        for o in self.objects:
+            o = str(o)
+            o = o.replace('{','')
+            o = o.replace('}','')
+            o = o.replace("'",'"')
+            o = o.replace(", ",',\n\t\t')
+            print(o)
+            new.append(o)
+        self.objects = new
+
+
+                    
+
 
 
 class TakeCSV:
@@ -24,7 +57,7 @@ class TakeCSV:
         self.filepath = filepath
         self.keys = []
         self.values = []
-        self.objs = []
+        self.getData()
 
     def getData(self):
         with open(self.filepath, 'r') as file_object:     
@@ -34,15 +67,12 @@ class TakeCSV:
                 self.values.append(x)
         self.keys = self.values[0]
         self.values.pop(0)
-        
-        for value in self.values:
-            x = JSONObject(self.keys,value).item
-            self.objs.append(x)
-            print(x)
 
-                
 
-    def showObjects(self):
-        self.getData()
+    
 
-TakeCSV('files/input.csv').showObjects()
+def main():
+    x = TakeCSV('files/input.csv')
+    JSONObject(x.keys,x.values,'files/output.json').writeToJSON()
+
+main()
